76bae6f886e68855738db7d7170bb0346a082508
fsb3rke <berkeeavcii@gmail.com>
2023-04-29 23:51:47 +0300

    Added new "input<T>(size)" function for make shorter form of the code. Added "getch()" message. (14::9)
    Our new "input<T>(size)" function has a demangling problem. So we'll be fixed on future updates.

--- main.cpp
+++ main.cpp
@@ -11,13 +11,25 @@ using namespace tabulate;
 #define EXIT_WITH_MESSAGE(condition, code, msg) \
     if (!(condition)) {                                         \
         std::cout<<msg<<std::endl; \
-        getch();                                            \
+        std::cout << "\n\n\n\nPress any key to close to the program..."; \
+        getch();                                       \
         std::exit(code);                        \
     }
 
-std::vector<Player> inputPlayers(const int& playerSize);
-std::vector<Team> inputTeams(const int& teamSize);
 
+// TODO: SOLVE DEMANGLING PROBLEM
+template<typename T>
+std::vector<T> input(const int& size) {
+    std::vector<T> vv;
+    for (int i = 0; i < size; ++i) {
+        std::string vvName;
+        std::cout << typeid(T).name() << " " << (i+1) << " : ";
+        std::cin >> vvName;
+        T pushVv(vvName);
+        vv.push_back(pushVv);
+    }
+    return vv;
+}
 
 int main() {
     srand(((unsigned) time(nullptr)));
@@ -27,10 +39,10 @@ int main() {
     std::cout << "Team Size: "; std::cin >> teamSize;
 
     // assert(playerSize < teamSize && "Team Size must be higher than Player Size");
-    EXIT_WITH_MESSAGE(playerSize < teamSize, -1, "Team Size must be higher than Player Size");
+    EXIT_WITH_MESSAGE(playerSize <= teamSize, -1, "Team Size must be higher than Player Size");
 
-    std::vector<Team> teams = inputTeams(teamSize);
-    std::vector<Player> players = inputPlayers(playerSize);
+    std::vector<Team> teams = input<Team>(teamSize);
+    std::vector<Player> players = input<Player>(playerSize);
 
     int teamIndex;
     teamIndex = 0;
@@ -59,6 +71,8 @@ int main() {
     for (Team& team : teams) {
         // add_cell_to_table(teamTable, 0, team.getTeamName());
         teamTable.add_row({ team.getTeamName() });
+        teamTable[0].format()
+            .font_color(Color::blue);
         for (Player& player : team.getPlayers()) {
             teamTable.add_row({ player.getPlayerName() });
         }
@@ -70,30 +84,4 @@ int main() {
     std::cout << "\n\n\n\nPress any key to close to the program...";
     getch();
     return 0;
-}
-
-std::vector<Player> inputPlayers(const int& playerSize) {
-    std::vector<Player> players;
-    for (int i = 0; i < playerSize; i++) {
-        std::string playerName;
-        std::cout << "Player " << (i+1) << " : ";
-        std::cin >> playerName;
-        Player pushPlayer(playerName);
-        players.push_back(pushPlayer);
-    }
-
-    return players;
-}
-
-std::vector<Team> inputTeams(const int& teamSize) {
-    std::vector<Team> teams;
-    for (int i = 0; i < teamSize; i++) {
-        std::string teamName;
-        std::cout << "Team " << (i+1) << " : ";
-        std::cin >> teamName;
-        Team pushTeam(teamName);
-        teams.push_back(pushTeam);
-    }
-
-    return teams;
 }
